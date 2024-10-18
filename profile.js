63036b097ec0e8d90868dc80295cead242ee868b
varshithteja <147536427+varshithteja@users.noreply.github.com>
2024-10-18 17:04:20 +0530

    Add files via upload

---
+++ profile.js
@@ -0,0 +1,9 @@
+// Smooth Scroll for navigation
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
